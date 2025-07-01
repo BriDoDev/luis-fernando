a8ff2585c3f5489e77e80f077b1be37502f63917
Brian Dominguez Vazquez <brian@Brian-PC.>
2025-07-01 17:40:17 -0600

    first version

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,14 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require("tailwindcss/defaultTheme");
 module.exports = {
-	content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-	theme: {
-		extend: {
-			fontFamily: {
-				sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
-			},
-		},
-	},
-	plugins: [require("@tailwindcss/typography")],
-	darkMode: "class",
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ["Inter Variable", "Inter", ...defaultTheme.fontFamily.sans],
+      },
+      keyframes: {
+        "gradient-shift": {
+          "0%, 100%": { backgroundPosition: "0% 50%" },
+          "50%": { backgroundPosition: "100% 50%" },
+        },
+      },
+      animation: {
+        "gradient-slow": "gradient-shift 5s ease infinite",
+      },
+      backgroundSize: {
+        200: "200% 200%",
+      },
+    },
+  },
+  plugins: [require("@tailwindcss/typography")],
+  darkMode: "class",
 };
+
