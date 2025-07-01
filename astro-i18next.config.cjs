d9c1ff56e55ccb530f6e0b0b68818951f71fc74d
Brian Dominguez Vazquez <brian@Brian-PC.>
2025-07-01 15:23:08 -0600

    base

---
+++ astro-i18next.config.cjs
@@ -0,0 +1,7 @@
+/** @type {import('astro-i18next').AstroI18nextConfig} */
+module.exports = {
+  defaultLocale: "es",
+  locales: ["en", "it", "es"],
+  routingStrategy: "prefix_except_default",
+};
+
